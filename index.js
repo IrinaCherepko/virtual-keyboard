ec7375a16ce9e379a5ad3f90fa080d1888f04170
IrinaCherepko <icheri87@gmail.com>
2023-04-30 15:20:52 +0300

    init: start task, add object into index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const letters = {Backquote:'ё', Digit1: '1', Digit1: '2', Digit1: '3', Digit1: '4', Digit1: '5', Digit1: '6', Digit1: '7', Digit1: '8', Digit1: '9', Digit1:'0', Minus: '-', Equal: '=', Backspace: 'Backspace', Tab: 'Tab', KeyQ: 'й', KeyW: 'ц',KeyE: 'у', KeyR: 'к',KeyT: 'е', KeyY: 'н', KeyU: 'г',KeyI: 'ш', KeyO: 'щ', KeyP: 'з', BracketLeft: 'х', BracketRight: 'ъ', Bracketlash: '\\', CapsLock: 'CapsLock', KeyA: 'ф', KeyS: 'ы', KeyD: 'в',KeyF: 'а', KeyG: 'п', KeyH: 'р', KeyJ: 'о', KeyK: 'л', KeyL: 'д', Semicolon: 'ж', Quote: 'э', Enter: 'Enter', ShiftLeft: 'Shift', KeyZ: 'я', KeyX: 'ч', KeyC: 'с', KeyV: 'м', KeyB: 'и', KeyN: 'т', KeyM: 'ь', Comma: 'б', Period: 'ю', Slash: '.', ShiftRight: 'Shift', ControlLeft: 'Control', MetaLeft: 'Meta', AltLeft: 'Alt', Space: ' ', ControlRight: 'Control', AltRight: 'Alt', ControlRight: 'Control', ArrowLeft: 'ArrowLeft', ArrowUp: 'ArrowUp', ArrowDown: 'ArrowDown', ArrowRight: 'ArrowRight'};
+
+letters.className = 'letters-class';
+letters.id = 'letters_id';
+
+function addClass(a){
+for(let key of letters){
+    
+}
+}
+addClass(letters);
