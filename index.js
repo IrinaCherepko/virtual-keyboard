8d6992cdc7e60f55d4a23d75eeed0b671e646632
IrinaCherepko <icheri87@gmail.com>
2023-05-01 12:53:49 +0300

    feat: add data in html file and add styles

--- index.js
+++ index.js
@@ -1,16 +1,109 @@
-const letters = {Backquote: ['ё', '`'], Digit1: ['1', '1'], Digit1: ['2', '2'], Digit1: ['3','3'], Digit1: ['4', '4'], Digit1: ['5', '5'], Digit1: ['6','6'], Digit1: ['7', '7'], Digit1: ['8', '8'], Digit1: ['9', '9'], Digit1:['0', '0'], Minus: ['-', '-'], Equal: ['=', '='], Backspace: ['Backspace', 'Backspace'], Tab: ['Tab', 'Tab'], KeyQ: ['й', 'q'], KeyW: ['ц', 'w'], KeyE: ['у', 'e'], KeyR: ['к', 'r'], KeyT: ['е', 't'], KeyY: ['н', 'y'], KeyU: ['г', 'u'], KeyI: ['ш', 'i'], KeyO: ['щ', 'o'], KeyP: ['з', 'p'], BracketLeft: ['х', '['], BracketRight: ['ъ', ']'], Bracketlash: ['\\', '\\'], CapsLock: ['CapsLock', 'CapsLock'], KeyA: ['ф', 'a'], KeyS: ['ы', 's'], KeyD: ['в', 'd'], KeyF: ['а', 'f'], KeyG: ['п', 'g'], KeyH: ['р', 'h'], KeyJ: ['о', 'j'], KeyK: ['л', 'k'], KeyL: ['д', 'l'], Semicolon: ['ж', ';'], Quote: ['э', ''], Enter: ['Enter', 'Enter'], ShiftLeft: ['Shift', 'Shift'], KeyZ: ['я', 'z'], KeyX: ['ч', 'x'], KeyC: ['с', 'c'], KeyV: ['м', 'v'], KeyB: ['и', 'b'], KeyN: ['т', 'n'], KeyM: ['ь', 'm'], Comma: ['б', ','], Period: ['ю','.'], Slash: ['.', '/'], ShiftRight: ['Shift', 'Shift'], ControlLeft: ['Control', 'Control'], MetaLeft: ['Meta', 'Meta'], AltLeft: ['Alt', 'Alt'], Space: [' ', ' '], ControlRight: ['Control', 'Control'], AltRight: ['Alt', 'Alt'], ControlRight: ['Control', 'Control'], ArrowLeft: ['ArrowLeft', 'ArrowLeft'], ArrowUp: ['ArrowUp', 'ArrowUp'], ArrowDown: ['ArrowDown', 'ArrowDown'], ArrowRight: ['ArrowRight', 'ArrowRight']};
+const letters = {
+    row1: {
+        Backquote: ['ё', '`'], 
+        Digit1: ['1', '1'], 
+        Digit2: ['2', '2'], 
+        Digit3: ['3','3'], 
+        Digit4: ['4', '4'], 
+        Digit5: ['5', '5'], 
+        Digit6: ['6','6'], 
+        Digit7: ['7', '7'], 
+        Digit8: ['8', '8'], 
+        Digit9: ['9', '9'], 
+        Digit0:['0', '0'], 
+        Minus: ['-', '-'], 
+        Equal: ['=', '='], 
+        Backspace: ['Backspace', 'Backspace'] 
+    },
+    row2: {
+        Tab: ['Tab', 'Tab'], 
+        KeyQ: ['й', 'q'], 
+        KeyW: ['ц', 'w'], 
+        KeyE: ['у', 'e'], 
+        KeyR: ['к', 'r'], 
+        KeyT: ['е', 't'], 
+        KeyY: ['н', 'y'], 
+        KeyU: ['г', 'u'], 
+        KeyI: ['ш', 'i'], 
+        KeyO: ['щ', 'o'], 
+        KeyP: ['з', 'p'], 
+        BracketLeft: ['х', '['], 
+        BracketRight: ['ъ', ']'],
+        Bracketlash: ['\\', '\\']
+    },
+    
+    row3: {
+        CapsLock: ['CapsLock', 'CapsLock'], 
+        KeyA: ['ф', 'a'], 
+        KeyS: ['ы', 's'], 
+        KeyD: ['в', 'd'], 
+        KeyF: ['а', 'f'], 
+        KeyG: ['п', 'g'], 
+        KeyH: ['р', 'h'], 
+        KeyJ: ['о', 'j'], 
+        KeyK: ['л', 'k'], 
+        KeyL: ['д', 'l'], 
+        Semicolon: ['ж', ';'], 
+        Quote: ['э', '\''], 
+        Enter: ['Enter', 'Enter'] 
+    },
+    
+    row4: {
+        CapsLock: ['CapsLock', 'CapsLock'], 
+        KeyA: ['ф', 'a'], 
+        KeyS: ['ы', 's'], 
+        KeyD: ['в', 'd'], 
+        KeyF: ['а', 'f'], 
+        KeyG: ['п', 'g'], 
+        KeyH: ['р', 'h'], 
+        KeyJ: ['о', 'j'], 
+        KeyK: ['л', 'k'], 
+        KeyL: ['д', 'l'], 
+        Semicolon: ['ж', ';'], 
+        Quote: ['э', ''], 
+        Enter: ['Enter', 'Enter']
+    },
 
-let tagLetters = document.body.insertAdjacentHTML('afterbegin', `<main>${letters}</main>`);
-tagLetters.className = 'letters-class';
-tagLetters.id = 'letters_id';
-document.body.prepend(tagLetters);
+    row5: {
+        ControlLeft: ['Control', 'Control'], 
+        MetaLeft: ['Meta', 'Meta'], 
+        AltLeft: ['Alt', 'Alt'], 
+        Space: [' ', ' '], 
+        ControlRight: ['Control', 'Control'], 
+        AltRight: ['Alt', 'Alt'], 
+        ControlRight: ['Control', 'Control'], 
+        ArrowLeft: ['ArrowLeft', 'ArrowLeft'], 
+        ArrowUp: ['ArrowUp', 'ArrowUp'], 
+        ArrowDown: ['ArrowDown', 'ArrowDown'], 
+        ArrowRight: ['ArrowRight', 'ArrowRight']
+    }    
+   
+};
 
-function addClass(a){
-for(let key in letters){
-    key.className = 'letter-class';
-    // key.innerHTML = '<div></div>';
-    console.log(key);
+let letters2 = [
+    [
+        {ControlLeft: ['Control', 'Control']},
+        {MetaLeft: ['Meta', 'Meta']},
+        {AltLeft: ['Alt', 'Alt']}
+    ]
+];
+
+
+class KeyBoard {
+
+    constructor(data)
+    {
+        this.root = document.getElementById('keyboard');
+        this.data = data;
+    }
+
+    create()
+    {
+
+        console.log(this.data)
+    }
+
 }
-}
-addClass(letters);
 
+let keyboard = new KeyBoard(letters2)
+keyboard.create()
