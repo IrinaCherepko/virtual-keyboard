13e37d6a43554bce3511045362464adb7053a7b0
IrinaCherepko <icheri87@gmail.com>
2023-05-11 12:18:15 +0300

    refactor

--- index.js
+++ index.js
@@ -1,69 +1,4 @@
 const letters = {
-    row1: {
-        Backquote: ['ё', '`'], 
-        Digit1: ['1', '1'], 
-        Digit2: ['2', '2'], 
-        Digit3: ['3','3'], 
-        Digit4: ['4', '4'], 
-        Digit5: ['5', '5'], 
-        Digit6: ['6','6'], 
-        Digit7: ['7', '7'], 
-        Digit8: ['8', '8'], 
-        Digit9: ['9', '9'], 
-        Digit0:['0', '0'], 
-        Minus: ['-', '-'], 
-        Equal: ['=', '='], 
-        Backspace: ['Backspace', 'Backspace'] 
-    },
-    row2: {
-        Tab: ['Tab', 'Tab'], 
-        KeyQ: ['й', 'q'], 
-        KeyW: ['ц', 'w'], 
-        KeyE: ['у', 'e'], 
-        KeyR: ['к', 'r'], 
-        KeyT: ['е', 't'], 
-        KeyY: ['н', 'y'], 
-        KeyU: ['г', 'u'], 
-        KeyI: ['ш', 'i'], 
-        KeyO: ['щ', 'o'], 
-        KeyP: ['з', 'p'], 
-        BracketLeft: ['х', '['], 
-        BracketRight: ['ъ', ']'],
-        Bracketlash: ['\\', '\\']
-    },
-    
-    row3: {
-        CapsLock: ['CapsLock', 'CapsLock'], 
-        KeyA: ['ф', 'a'], 
-        KeyS: ['ы', 's'], 
-        KeyD: ['в', 'd'], 
-        KeyF: ['а', 'f'], 
-        KeyG: ['п', 'g'], 
-        KeyH: ['р', 'h'], 
-        KeyJ: ['о', 'j'], 
-        KeyK: ['л', 'k'], 
-        KeyL: ['д', 'l'], 
-        Semicolon: ['ж', ';'], 
-        Quote: ['э', '\''], 
-        Enter: ['Enter', 'Enter'] 
-    },
-    
-    row4: {
-        CapsLock: ['CapsLock', 'CapsLock'], 
-        KeyA: ['ф', 'a'], 
-        KeyS: ['ы', 's'], 
-        KeyD: ['в', 'd'], 
-        KeyF: ['а', 'f'], 
-        KeyG: ['п', 'g'], 
-        KeyH: ['р', 'h'], 
-        KeyJ: ['о', 'j'], 
-        KeyK: ['л', 'k'], 
-        KeyL: ['д', 'l'], 
-        Semicolon: ['ж', ';'], 
-        Quote: ['э', ''], 
-        Enter: ['Enter', 'Enter']
-    },
-
     row5: {
         ControlLeft: ['Control', 'Control'], 
         MetaLeft: ['Meta', 'Meta'], 
@@ -82,9 +17,82 @@ const letters = {
 
 let letters2 = [
     [
-        {ControlLeft: ['Control', 'Control']},
-        {MetaLeft: ['Meta', 'Meta']},
-        {AltLeft: ['Alt', 'Alt']}
+        {Backquote: ['ё', '`']}, 
+        {Digit1: ['1', '1']}, 
+        {Digit2: ['2', '2']}, 
+        {Digit3: ['3','3']}, 
+        {Digit4: ['4', '4']}, 
+        {Digit5: ['5', '5']}, 
+        {Digit6: ['6','6']}, 
+        {Digit7: ['7', '7']}, 
+        {Digit8: ['8', '8']}, 
+        {Digit9: ['9', '9']}, 
+        {Digit0:['0', '0']}, 
+        {Minus: ['-', '-']}, 
+        {Equal: ['=', '=']}, 
+        {Backspace: ['Backspace', '']} 
+    ],
+    [
+        {Tab: ['Tab', '']}, 
+        {KeyQ: ['й', 'q']}, 
+        {KeyW: ['ц', 'w']}, 
+        {KeyE: ['у', 'e']}, 
+        {KeyR: ['к', 'r']}, 
+        {KeyT: ['е', 't']}, 
+        {KeyY: ['н', 'y']}, 
+        {KeyU: ['г', 'u']}, 
+        {KeyI: ['ш', 'i']}, 
+        {KeyO: ['щ', 'o']}, 
+        {KeyP: ['з', 'p']}, 
+        {BracketLeft: ['х', '[']}, 
+        {BracketRight: ['ъ', ']']},
+        {Bracketlash: ['\\', '\\']}
+    ],
+    [
+        {CapsLock: ['CapsLock', '']}, 
+        {KeyA: ['ф', 'a']}, 
+        {KeyS: ['ы', 's']}, 
+        {KeyD: ['в', 'd']}, 
+        {KeyF: ['а', 'f']}, 
+        {KeyG: ['п', 'g']}, 
+        {KeyH: ['р', 'h']}, 
+        {KeyJ: ['о', 'j']}, 
+        {KeyK: ['л', 'k']}, 
+        {KeyL: ['д', 'l']}, 
+        {Semicolon: ['ж', ';']}, 
+        {Quote: ['э', '\'']}, 
+        {Enter: ['Enter', '']} 
+    ],
+    [
+        {ShiftLeft: ['Shift', '']}, 
+        {KeyZ: ['я', 'z']}, 
+        {KeyX: ['ч', 'x']}, 
+        {KeyC: ['с', 'c']}, 
+        {KeyV: ['м', 'v']}, 
+        {KeyB: ['и', 'b']}, 
+        {KeyN: ['т', 'n']}, 
+        {KeyM: ['ь', 'm']}, 
+        {Comma: ['б', ',']}, 
+        {Period: ['ю', '.']}, 
+        {Slash: ['.', '/']}, 
+        {ShiftRight: ['Shift', '']}, 
+    ],
+    [
+        {ControlLeft: ['Control', '']}, 
+        {MetaLeft: ['Meta', '']}, 
+        {AltLeft: ['Alt', '']}, 
+        {Space: [' ', ' ']}, 
+        {ControlRight: ['Control', '']}, 
+        {AltRight: ['Alt', '']}, 
+        {ControlRight: ['Control', '']},
+        {
+            Arrow: {
+                ArrowLeft: ['ArrowLeft', '&larr;'], 
+                ArrowUp: ['ArrowUp', '&uarr;'], 
+                ArrowDown: ['ArrowDown', '&darr;'], 
+                ArrowRight: ['ArrowRight', '&rarr;']
+            }
+        },
     ]
 ];
 
@@ -93,14 +101,38 @@ class KeyBoard {
 
     constructor(data)
     {
-        this.root = document.getElementById('keyboard');
+        this.root = document.getElementById('keyboard2');
         this.data = data;
     }
 
     create()
     {
-
+        this.row(0, "one-row");
+        this.row(1, "two-row");
+        this.row(2, "three-row");
+        this.row(3, "four-row"); 
         console.log(this.data)
+    }
+
+    row(k, row_class)
+    {
+        this.root.insertAdjacentHTML('beforeEnd', `<div class="row ${row_class}"></div>`)
+        let one = this.root.querySelector(`.${row_class}`);
+        this.data[k].forEach(element => {
+            for (const key in element) {
+                if(key != "Arrow") {
+                    one.insertAdjacentHTML('beforeEnd', `
+                    <div class="letter" data-key="${key}">
+                        <div class="top">${element[key][0]}</div>
+                        <div class="bottom">${element[key][1]}</div>
+                    </div>
+                    `)
+                } else {
+                    
+                }
+                
+            }
+        });
     }
 
 }
